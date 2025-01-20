c539c33186357f0452a90351d09f468ae78279ac
lyderic <lyderic@balzac.lyderic.net>
2025-01-20 21:09:08 +0100

    improved install-packages script

--- run_once_install-packages.bash
+++ run_once_install-packages.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 main() {
+	[ $(id -u) -eq 0 ] || sudo="sudo"
 	[ -e /etc/os-release ] && {
-		DISTRO=$(grep -E '^ID=' /etc/os-release | cut -d'=' -f2)
+		DISTRO=$(awk -F= '/^ID=/ {print $2}' /etc/os-release)
 	}
 	echo -e "Distribution: \e[33m${DISTRO}\e[m"
 	case "${DISTRO}" in
-	"ubuntu" | "debian") ubuntu ;;
-	"arch" | "archarm") arch ;;
-	"alpine") alpine ;;
-	*) die "${DISTRO}: only archlinux, ubuntu and alpine are supported" ;;
+		"ubuntu" | "debian") ubuntu ;;
+		"arch" | "archarm") arch ;;
+		"alpine") alpine ;;
+		*) die "${DISTRO}: only archlinux, ubuntu and alpine are supported" ;;
 	esac
 	echo -e "\e[36mbase packages installed\e[m"
 }
@@ -19,32 +20,32 @@ arch() {
 	header "pacman setup"
 	for item in ParallelDownloads Color; do
 		grep "^${item}" /etc/pacman.conf || {
-			sudo sed -i -e "s/^#${item}/${item}/" /etc/pacman.conf
+			$sudo sed -i -e "s/^#${item}/${item}/" /etc/pacman.conf
 			warn "${item} uncommented in /etc/pacman.conf"
 		}
 	done
 	header "arch packages"
-	sudo pacman -S --noconfirm --needed ${packages}
+	$sudo pacman -S --noconfirm --needed ${packages}
 }
 
 ubuntu() {
 	local packages="${COMMON_PACKAGES} ${UBUNTU_PACKAGES}"
 	header "ubuntu packages"
-	sudo apt-get update && sudo apt-get -y install ${packages}
+	$sudo apt-get update && $sudo apt-get -y install ${packages}
 	# As of 22.04, just is not in ubuntu's repos :(
 	[ -x /usr/local/bin/just ] || {
 		curl -sL --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh |
-			sudo bash -s -- --to /usr/local/bin && ok "just successfully installed"
+			$sudo bash -s -- --to /usr/local/bin && ok "just successfully installed"
 	}
 	# bat is in ubuntu's repo but gets installed as 'batcat'
-	[ -x /usr/bin/bat ] || sudo ln -s /usr/bin/batcat /usr/bin/bat
+	[ -x /usr/bin/bat ] || $sudo ln -s /usr/bin/batcat /usr/bin/bat
 }
 
 alpine() {
 	local packages="${COMMON_PACKAGES} ${ALPINE_PACKAGES}"
 	header "alpine packages"
-	sudo apk -U upgrade
-	sudo apk add ${packages}
+	$sudo apk -U upgrade
+	$sudo apk add ${packages}
 }
 
 # DISPLAY
@@ -58,52 +59,21 @@ die() {
 
 # Packages that are common to arch, ubuntu and alpine
 COMMON_PACKAGES="
-bash
-bash-completion
-bat
-curl
-direnv
-file
-git
-htop
-sudo
-tmux
-vim
+bash bash-completion bat curl direnv file git htop sudo
+tmux vim
 "
 
 # Additional packages for archlinux
 ARCH_PACKAGES="
-croc
-diffutils
-duf
-fzf
-gdu
-grep
-just
-less
-pacman-contrib
-the_silver_searcher
-tree
-which
+croc diffutils duf fzf gdu grep just less pacman-contrib the_silver_searcher tree which
 "
 
 # Additional packages for ubuntu/debian
-UBUNTU_PACKAGES="
-grep
-less
-silversearcher-ag
-tree
-"
+UBUNTU_PACKAGES="grep less silversearcher-ag tree"
 
 # Additional packages for alpine
 ALPINE_PACKAGES="
-croc
-diffutils
-fzf
-gdu
-just
-the_silver_searcher
-which
+croc diffutils fzf gdu just the_silver_searcher which
 "
 
 main ${@}

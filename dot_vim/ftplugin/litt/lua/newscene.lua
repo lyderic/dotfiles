bc24f142fe2fbed6b707e5d7df4c29175ea03a32
lyderic <lyderic@balzac.lyderic.net>
2025-04-17 10:53:54 +0200

    vim litt: improved newscene lua script

--- dot_vim/ftplugin/litt/lua/newscene.lua
+++ dot_vim/ftplugin/litt/lua/newscene.lua
@@ -1,14 +1,16 @@
+local e = io.popen
 local grepcmd = "grep -siIrhoE '^\\[//[0-9a-f]{4}\\]' *"
+local gencmd = "uuidgen | head -c4"
 
 -- for practical reasons, id is in the form: '[//xxxx]', eg: [//143f]
 function main()
 	::begin::
-	local uuid = io.popen("uuidgen | head -c4"):read()
+	local uuid = e(gencmd):read()
 	local id = '[//'..uuid..']'
 	-- we check that there is no uuid collision as the uuid is too
 	-- short (only 4 chars). uuid is short because we want to be able
 	-- to type it quickly if needed.
-	for existing_id in io.popen(grepcmd):lines() do
+	for existing_id in e(grepcmd):lines() do
 		if existing_id == id then goto begin end
 	end
 	vim.command("normal o"..id..": # (")

4431a71a056a2c2e2354b598d75aef897d4e28ae
lyderic <lyderic@balzac.lyderic.net>
2025-07-09 08:34:54 +0200

    improved vigilax

--- dot_justfile.d/executable_vigilax.lua
+++ dot_justfile.d/executable_vigilax.lua
@@ -2,11 +2,10 @@
 
 local leepath = os.getenv("HOME").."/.local/share/lua/?.lua"
 package.path = package.path..";"..leepath
-
 require "lee"
-utsname = require "posix.sys.utsname"
 
 local m = {}
+local hinfo = json.decode(ea("hostnamectl --json=short status"))
 
 function main()
 	init()
@@ -15,43 +14,16 @@ function main()
 	updates()
 	diskusage()
 	coc()
-	local content = json.encode(m, { indent = true })
-	print(content)
+	print(json.encode(m, { indent = true }))
 end
 
 function init()
-	unameinfo = utsname.uname()
-	m.hostname = unameinfo.nodename
-	running_kernel = unameinfo.release
-	running_kernel = running_kernel:gsub(".lts","")
-	m.rkernel = running_kernel:gsub("-",".")
-	m.distro = distro()
-	m.mtype = eo("systemd-detect-virt")
-end
-
-function distro()
-	local fh = io.open("/etc/os-release")
-	for line in fh:lines() do
-		if line:match("^ID=") then
-			d = line:sub(4):gsub('"', '') fh:close() break
-		end
-	end
-	return d
-end
-
-function reboot()
-	m.reboot = false
-	if m.mtype == "lxc" then return end
-	if m.distro == "arch" then
-		local cmd = "pacman -Q linux linux-lts"
-		cmd = cmd .. " 2>/dev/null | awk '{print $2}'"
-		local output = eo(cmd)
-		if not output then return end
-		m.ikernel = output:gsub("-",".")
-		if m.rkernel ~= m.ikernel then m.reboot = true end
-	elseif m.distro == "ubuntu" then
-		if abs("/var/run/reboot-required") then m.reboot = true end
-	end
+	m.hostname = hinfo.Hostname
+	m.hardware = hinfo.HardwareVendor.." "..hinfo.HardwareModel
+	m.chassis = hinfo.Chassis
+	m.rkernel = knorm(hinfo.KernelRelease)
+	m.distro = hinfo.OperatingSystemPrettyName
+	m.virt = eo("systemd-detect-virt")
 end
 
 function uptime()
@@ -79,17 +51,28 @@ function diskusage()
 	local just = "just --justfile "..env("HOME").."/justfile"
 	local targets = eo(just.." --evaluate targets 2>/dev/null")
 	if not targets then targets = "/" end
-	cmd = "df --output=target,pcent "..targets
+	local cmd = f("df --output=target,pcent %s | sed 1d", targets)
 	for line in e(cmd):lines() do
-		if line:sub(1,1) ~= '/' then goto next end
-		local entry = {}
-		entry.mountpoint, entry.pcent = line:match('(%S+)%s+(%S+)')
-		table.insert(m.diskusage, entry)
+		mountpoint, pcent = line:match('(%S+)%s+(%S+)')
+		m.diskusage[mountpoint] = tonumber(pcent:sub(1,-2))
 	::next:: end
 end
 
+function reboot()
+	if m.mtype == "lxc" then return end
+	if m.distro:lower():sub(1,4) == "arch" then
+		local cmd = "pacman -Q linux linux-lts"
+		cmd = cmd .. " 2>/dev/null | awk '{print $2}'"
+		local ikernel = eo(cmd)
+		if not ikernel then return end
+		m.ikernel = knorm(ikernel)
+		if m.rkernel ~= m.ikernel then m.reboot = true end
+	elseif m.distro == "ubuntu" then
+		if abs("/var/run/reboot-required") then m.reboot = true end
+	end
+end
+
 function coc()
-	m.coc = "n/a"
 	if abs("/usr/local/bin/coc") then
 		if x("mountpoint -q /pool") then
 			m.coc = "loaded"
@@ -97,6 +80,11 @@ function coc()
 			m.coc = "unloaded"
 		end
 	end
+end
+
+function knorm(kernel)
+	local k = kernel:gsub(".lts","")
+	return k:gsub("-",".")
 end
 
 main()

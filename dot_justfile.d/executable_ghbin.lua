362f1329721277c668a78fbba742a7caf2c4de22
lyderic <lyderic@balzac.lyderic.net>
2025-11-19 08:14:58 +0100

    added --quiet option to ghbin

--- dot_justfile.d/executable_ghbin.lua
+++ dot_justfile.d/executable_ghbin.lua
@@ -8,11 +8,12 @@ require "lee"
 url = "https://lola.lyderic.com"
 
 function main()
+	if arg[1] == "--quiet" then quiet = true end
 	local cmd = f("curl -sL %s/cgi-bin/state", url)
 	local state = json.decode(ea(cmd))
 	for _,i in ipairs(state) do
 		printf("\27[1m%-8.8s  \27[m", i.binary..":")
-		local c = get_situation(i.binary)
+		local c = get_situation(i.binary, quiet)
 		if not c then goto next end
 		if c.parent ~= "/usr/local/bin/" then
 			printf("\27[90mskipped as found in %q\27[m\n", c.parent)
@@ -31,13 +32,13 @@ end
 -- get situation of a binary on the host, i.e.path, parent, cksum
 -- and size
 -- if a binary is not found, it is deployed
-function get_situation(binary)
+function get_situation(binary, quiet)
 	local t = {} -- local situation on host
 	t.binary = binary
 	t.path = eo("command -v "..binary)
 	if not t.path then
 		printf("\27[33m%s: binary not found\n\27[m", binary)
-		deploy(binary)
+		deploy(binary, quiet)
 		return nil
 	end
 	t.parent = t.path:match(".*/")
@@ -48,11 +49,12 @@ end
 -- if binary not installable with pacman, then get binary from lola
 -- and put it in /usr/local/bin
 -- if binary already found in /usr/local/bin, it is overwritten
-function deploy(binary)
+function deploy(binary, quiet)
 	printf("\27[7m %s \27[m", binary)
 	if pacman(binary) then return end
 	local gz = binary..".gz"
-	local curl_cmd = f("curl -L -o %q %s/gz/%s", "/tmp/"..gz, url, gz)
+	local curl_call = f("curl %s", quiet and "-s" or "")
+	local curl_cmd = f("%s -L -o %q %s/gz/%s", curl_call, "/tmp/"..gz, url, gz)
 	print("---> [XeQ] ", curl_cmd)
 	if not x(curl_cmd) then return end
 	if not x("gzip -d /tmp/"..gz) then return end
